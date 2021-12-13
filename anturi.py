cf41779e4bf020d3e1f2577d5d2f697c673eb4bc
Pasi <91182746+Salpaus@users.noreply.github.com>
2021-12-13 09:41:03 +0200

    paivitykset

--- anturi.py
+++ anturi.py
@@ -1,33 +1,31 @@
+##Tämä koodi ottaa anturista dataa ja siirtää sen MYSQL tietokantaan 
+
 import time
 from datetime import datetime
 import mariadb
-
-##salasana = "testi123"
-##password = 0
-##while password == 0:
-##    salasana2 = input("Anna salasana: ")
-##    if salasana2 == salasana:
-##        print("Password correct")
-##        password == 1
-##        break
-##    
-##    elif salasana2 != salasana:
-##        print("Password incorrect")
-##        password == 0
-        
-##while password == 1:
+import Adafruit_DHT
 
 conn = mariadb.connect(user="root", password="HyTe", host="localhost", database="Tiedot")
 cur = conn.cursor()
 
 arvo = 2
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4
+
+##Koodissa on 5 sekunnin loop, jonka aikana koodi siirtää anturidatan ja sen hetkisen kellonajan, jos anturi ei saa tietoa se tulostaa "Ei tietoa"
 
 while True:
     time.sleep(5)
-    print(f"{datetime.now()}".split('.')[0])
-    cur.execute(f"INSERT INTO Mittari (arvo, pvm) VALUES ('{arvo}', '{datetime.now()}')")
-    arvo += 2
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if temperature is not None:
+        print("Temp={0:0.1f}C".format(temperature))
+        aika = (f"{datetime.now()}".split('.')[0])
+        print(aika)
+        cur.execute(f"INSERT INTO Mittari (arvo, pvm) VALUES ('{temperature}', '{aika}')")
+        arvo += 2
+    else:
+        print("Ei tietoa")
     conn.commit()
-    
+
 conn.close()
 
